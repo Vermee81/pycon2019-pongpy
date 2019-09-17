77fca47016a8ace77735ddfa64b421abc4b6154b
Vermee81 <hrksb5029@gmail.com>
2019-09-17 14:50:26 +0900

    [update] バーに到達する位置を予測してバーを移動する

--- challenger.py
+++ challenger.py
@@ -3,12 +3,71 @@ import os
 from pongpy.interfaces.team import Team
 from pongpy.models.game_info import GameInfo
 from pongpy.models.state import State
+from pongpy.definitions import HEIGHT
 
 
 PLAYER_NAME = os.environ['PLAYER_NAME']
 
 
 class ChallengerTeam(Team):
+    prev_ball_pos_y = 0
+    prev_ball_pos_x = 0
+
+    def calc_end_pos_y(self, ball_x, ball_y, bar_pos_x):
+        """
+        一つ前のフレームのボールの位置と現在のボールの位置の差分をとって、最終的にどこの端に行き着くか予測する。
+    
+        Parameters
+        ----------
+        ball_x: int 現在のボールのx座標
+        ball_y: int 現在のボールのy座標
+        bar_pos_x: バーの位置のx座標
+    
+        Returns
+        -------
+        end_pos_y : int 端のy座標の位置
+
+        """
+
+        # 左上が0,0 下に行くほどy、右に行くほどxが増加する
+        # 傾きと切片を計算する
+        # y = slant * x + y_inter
+        #
+        if self.prev_ball_pos_x - ball_x == 0:
+            return ball_y
+        slant = (self.prev_ball_pos_y - ball_y) / (self.prev_ball_pos_x - ball_x)
+
+        # 何回壁に反射するか計算する
+
+        # barに到達するまでのフレーム数
+        to_frames = abs(bar_pos_x - ball_x) / abs(self.prev_ball_pos_x - ball_x)
+        # 反射する数
+        bounces = (to_frames * abs(self.prev_ball_pos_y - ball_y)) / HEIGHT + 1
+
+        # ボールがバーの位置に達する時の式を導く
+        # 現時点のx切片からバーに到達するまでに反射する回数 / 2回 分、バーに近づけるとバーに到達するときのx切片になるはず
+        # y切片を計算する
+        y_inter = ball_y - slant * ball_x
+
+        # 現時点のx切片を求める
+        x_inter = -1 * y_inter / slant
+
+        x_inter += x_inter * int(bounces / 2)
+
+        y_inter = -1 * int(bounces / 2) * slant * x_inter
+
+        # バーのxの位置の時のyの位置を計算する
+        end_pos_y = bar_pos_x * slant + y_inter
+
+        print("y = ", slant, " x + ", y_inter)
+        print("end_pos_y ", end_pos_y)
+
+        # 最後は1つ前のボールの位置を更新する
+        self.prev_ball_pos_y = ball_y
+        self.prev_ball_pos_x = ball_x
+
+        return end_pos_y
+
     @property
     def name(self) -> str:
         return PLAYER_NAME
@@ -17,10 +76,14 @@ class ChallengerTeam(Team):
         '''
         前衛の青色のバーをコントロールします。
         '''
-        return 1
+        end_y = self.calc_end_pos_y(state.ball_pos.x, state.ball_pos.y, state.mine_team.atk_pos.x)
+        direction = (state.ball_pos.y - state.mine_team.atk_pos.y) > 0
+        return info.atk_return_limit if direction else -info.atk_return_limit
 
     def def_action(self, info: GameInfo, state: State) -> int:
         '''
         後衛のオレンジ色のバーをコントロールします。
         '''
-        return 1
+        end_y = self.calc_end_pos_y(state.ball_pos.x, state.ball_pos.y, state.mine_team.atk_pos.x)
+        direction = (end_y - state.mine_team.def_pos.y) > 0
+        return info.def_return_limit if direction else -info.def_return_limit
